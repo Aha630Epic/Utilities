17da8e1d0f3de2b10ec285d75eb2397fb7f35191
Aha630Epic <121641938+Aha630Epic@users.noreply.github.com>
2023-01-01 12:49:10 -0800

    aaaaaaaaaa

---
+++ Crucifixs Folder/Plushie Loader.lua
@@ -0,0 +1,8 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/Ambush%20Plushie.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/Christmas%20Screech%20Plushie.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/Figure%20Plushie.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/Jack%20Plushie.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/Rush%20Plushie.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/Seek%20Plushie.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/Screech%20Plushie.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aha630Epic/Utilities/main/Crucifixs%20Folder/A-60%20Plushie.lua"))();

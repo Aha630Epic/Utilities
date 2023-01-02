500513c38212bc62167e50abebb7264798b31f1b
Aha630Epic <121641938+Aha630Epic@users.noreply.github.com>
2023-01-01 16:44:54 -0800

    a

--- Crucifixs Folder/A-60 Plushie.lua
+++ Crucifixs Folder/A-60 Plushie.lua
@@ -86,7 +86,7 @@ end
 
 -- Scripts
 
-local CrucifixTool = game:GetObjects("rbxassetid://11996702987")[1]
+local CrucifixTool = game:GetObjects("rbxassetid://11998395746")[1]
 CrucifixTool.Name = "Crucifix"
 CrucifixTool.Parent = game.Players.LocalPlayer.Backpack
 
@@ -94,7 +94,7 @@ CrucifixTool.Parent = game.Players.LocalPlayer.Backpack
 --     if proc then return end
 
 --     if input.KeyCode == input.KeyCode[getgenv().spawnKey] then
---         local CrucifixTool = game:GetObjects("rbxassetid://11996702987")[1]
+--         local CrucifixTool = game:GetObjects("rbxassetid://11998395746")[1]
 --         CrucifixTool.Name = "Crucifix"
 --         CrucifixTool.Parent = game.Players.LocalPlayer.Backpack
 --     end
